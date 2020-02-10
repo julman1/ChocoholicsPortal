028eece28a9062d168b89d504514c3285346b1ed
julman1 <39272725+julman1@users.noreply.github.com>
2020-02-10 15:43:30 -0500

    Initial Commit
    Outlined the login and portal page.

---
+++ ChocoholicsPortal/ChocoholicsPortal/VerifyID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocoholicsPortal
+{
+    class VerifyID
+    {
+        public bool CheckTheID(string ID)
+        {
+            if(string.IsNullOrEmpty(ID))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
